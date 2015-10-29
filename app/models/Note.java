803a2a093b3456bc2dde2803aabfed5f686bd5a7
rockguy <rockguy112@gmail.com>
2015-10-29 23:00:31 +0800

    Sheet happen  Please enter the commit message for your changes. Lines starting

---
+++ app/models/Note.java
@@ -0,0 +1,34 @@
+package models;
+
+/**
+ *
+ */
+import java.util.*;
+import play.data.validation.Constraints.*;
+import javax.persistence.*;
+import play.db.ebean.Model;
+
+@Entity
+public class Note extends Model {
+
+    @Id
+    public Long id;
+    @Required
+    public String name;
+    public String cellPhone;
+    public  String homePhone;
+
+    public static Finder<Long, Note> find = new Finder<Long, Note>(
+            Long.class, Note.class
+    );
+
+    public static List<Note> all() {
+        return find.all();
+    }
+
+    @Deprecated
+    public static void edit(Note note,Long id) {
+        note.update(id);
+    }
+
+}
